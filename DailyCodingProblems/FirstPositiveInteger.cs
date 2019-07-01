118054d1c7a3184c29f8acc159465d691177b616
turtlewaxer <turtlewaxer1100@gmail.com>
2019-07-01 01:38:30 -0400

    Daily coding problem for 6-30-19

---
+++ DailyCodingProblems/FirstPositiveInteger.cs
@@ -0,0 +1,57 @@
+﻿/*
+	Given an array of integers, find the first missing positive integer in linear time and constant space.
+	In other words, find the lowest positive integer that does not exist in the array.
+	The array can contain duplicates and negative numbers as well.
+
+	For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+	You can modify the input array in-place.
+*/
+
+using System;
+
+namespace DailyCodingProblems
+{
+	public class FirstPositiveInteger
+	{
+		public static int FindFirstPositiveInteger(int[] set)
+		{
+			var start = OffsetNegatives(set);
+
+			for (var i = start; i < set.Length; i++) {
+				var num = Math.Abs(set[i]);
+				if (num <= set.Length - start)
+				{
+					var index = start + num - 1;
+					if (index >= 0)
+					{
+						set[index] = -1 * Math.Abs(set[index]);
+					}
+				}
+			}
+
+			for (var i = start; i < set.Length; i++)
+			{
+				if (set[i] > 0) {
+					return i - start + 1;
+				}
+			}
+
+			return set.Length - start + 1;
+		}
+
+		private static int OffsetNegatives(int[] set) {
+			var j = 0;
+			for (int i = 0; i < set.Length; i++) {
+				if (set[i] <= 0) {
+					var temp = set[i];
+					set[i] = set[j];
+					set[j] = temp;
+					j++;
+				}
+			}
+
+			return j;
+		}
+	}
+}
---
+++ SolutionTest/FirstPositiveIntegerTest.cs
@@ -0,0 +1,55 @@
+﻿using DailyCodingProblems;
+using System.Linq;
+using Xunit;
+
+namespace SolutionTest
+{
+	public class FirstPositiveIntegerTest
+	{
+		[Fact]
+		public void Test1()
+		{
+			Evaluate(new int[] { 3, 4, -1, 1 }, 2);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			Evaluate(new int[] { 1, 2, 0 }, 3);
+		}
+
+		[Fact]
+		public void Test3()
+		{
+			Evaluate(new int[] { 1, 2, 0, 4, 10, -9, -100 }, 3);
+		}
+
+		[Fact]
+		public void Test4()
+		{
+			Evaluate(new int[] { 0 }, 1);
+		}
+
+		[Fact]
+		public void Test5()
+		{
+			Evaluate(new int[] { -1, 0, -100 }, 1);
+		}
+
+		[Fact]
+		public void Test6()
+		{
+			Evaluate(new int[] { 1, 2, 3, 5 }, 4);
+		}
+
+		[Fact]
+		public void Test7()
+		{
+			Evaluate(new int[0], 1);
+		}
+
+		private void Evaluate(int[] set, int expected) {
+			Assert.Equal(expected, FirstPositiveInteger.FindFirstPositiveInteger(set));
+		}
+	}
+}
